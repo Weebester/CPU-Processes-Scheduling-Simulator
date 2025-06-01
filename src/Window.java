3021c624f116af0f3afa345d527fec35b6a8db4d
Karrar <krara.2003@gmail.com>
2025-06-01 19:39:09 +0300

    commit

--- src/Window.java
+++ src/Window.java
@@ -7,7 +7,7 @@ public class Window extends JFrame {
 
 
     Window(){
-        this.setTitle("Processes schedule calculator");
+        this.setTitle("Processes schedule simulator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1280, 720);
         this.add(new UI());
